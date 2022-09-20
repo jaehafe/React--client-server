930354ad9dd56d3c9280f282aafd96217f037e7c
jaeha <wogkdkrm112@gmail.com>
2022-09-20 14:35:06 +0900

    privacy

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded 이렇게 된 data를 분석해서 가져옴
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 'json type'으로 된 것을 분석해서 가져올 수 있게
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose
-  .connect(
-    'mongodb+srv://wogkdkrm:wogkdkrm@login.yl8h8vj.mongodb.net/?retryWrites=true&w=majority'
-  )
+  .connect(config.mongoURI)
   .then(() => {
     console.log('MongoDB connected');
   })
@@ -15,8 +22,20 @@ mongoose
     console.log(err);
   });
 
-app.get('/', (req, res) => {
-  res.send('welcome');
+app.get('/', (req, res) => res.send('welcome!!'));
+
+app.post('/register', (req, res) => {
+  // 회원가입시 필요한 정보들을 client에서 get하면
+  // DB에 넣어준다
+
+  const user = new User(req.body); // bodyParser를 이용해 requset body로 client에서 받아온 정보를 받아준다.
+  // save -> mongodb method
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
